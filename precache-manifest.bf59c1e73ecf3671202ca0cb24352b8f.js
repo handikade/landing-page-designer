34713c84e039878b9ea1c6abd2fc5e5194aa0cc1
Dika Handika <dika.csui@gmail.com>
2019-12-02 09:36:21 +0700

    Updates

---
+++ precache-manifest.bf59c1e73ecf3671202ca0cb24352b8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffa48ec8b03af31be7354a0b48d786b",
+    "url": "/landing-page-designer/index.html"
+  },
+  {
+    "revision": "10625a1fb5ac7c66c15e",
+    "url": "/landing-page-designer/static/css/main.b0b8072b.chunk.css"
+  },
+  {
+    "revision": "b3c652f6234557966cc9",
+    "url": "/landing-page-designer/static/js/2.d0550553.chunk.js"
+  },
+  {
+    "revision": "10625a1fb5ac7c66c15e",
+    "url": "/landing-page-designer/static/js/main.fc3c0001.chunk.js"
+  },
+  {
+    "revision": "a1668f81c335f3805530",
+    "url": "/landing-page-designer/static/js/runtime-main.7cb10782.js"
+  },
+  {
+    "revision": "a5a74b4ff9be5b81b4ee10c0f456d8e9",
+    "url": "/landing-page-designer/static/media/work-desk__dustin-lee.a5a74b4f.jpg"
+  }
+]);
