659ab85c9b461205ea6cf6358c9eef5c165ad481
Dika Handika <dika.csui@gmail.com>
2019-12-02 13:10:24 +0700

    penyesuain semantic list

--- src/components/highlight/HighlightBox.js
+++ src/components/highlight/HighlightBox.js
@@ -4,11 +4,11 @@ import './HighlightBox.scss'
 
 function HighlightBox({ title, icon, content }) {
   return (
-    <div className="highlight-box">
+    <li className="highlight-box">
       <div className="highlight-box__title">{title}</div>
       <div className="highlight-box__icon">{icon}</div>
       <div className="highlight-box__content">{content}</div>
-    </div>
+    </li>
   )
 }
 
